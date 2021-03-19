87ba6b4a6c2afeb29537d2eb8bcabf1f45064004
McDufis <McDufis@mcdufis.com>
2021-03-19 11:50:49 -0400

    First Commit

---
+++ Source Management 1/Source Management 1.cpp
@@ -0,0 +1,18 @@
+//Name: Jesse Picker
+//Date: x/x/2020
+//Project Name: 
+//Project Description:
+
+//Libraries
+#include<iostream>
+
+//Program Structure
+using namespace std;
+
+int main() 
+{
+	cout << "Hello, World!" << endl;
+	
+	system("pause");
+	return 0;
+}
